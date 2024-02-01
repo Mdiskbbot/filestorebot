7554c6c1c3962438c7b9819e80a39cc75472af77
Mdiskbbot <158266051+Mdiskbbot@users.noreply.github.com>
2024-02-01 14:48:33 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,19 +1,19 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
+  API_ID = int(os.environ.get("API_ID", "29732337"))
+  API_HASH = os.environ.get("API_HASH", "27c900a8bac51da6d0fd91aad09ef779")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "v2filestore_mbbot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002045030978"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "Shareus.io")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "7ihzpBcPx9Yjg0IGfCa8W5NsKqj1)
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "5885149906"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://worksoheb:worksoheb@autofiltermbbot.yskcmue.mongodb.net/?retryWrites=true&w=majority")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002102320020")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002022321932"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
-  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
+  FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", False))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
   BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "").split()))
   OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
@@ -23,7 +23,7 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 
 ╭────[ 🔅FɪʟᴇSᴛᴏʀᴇBᴏᴛ🔅]────⍟
 │
-├🔸 My Name: [FileStore Bot](https://t.me/{BOT_USERNAME})
+├🔸 My Name: [FileStore Bot](https://t.me/v2filestore_mbbot)
 │
 ├🔸 Language: [Python 3](https://www.python.org)
 │
@@ -32,11 +32,11 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [VJ](https://telegram.me/KingVj01)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [Jack Sparrow](https://telegram.me/mdiskblast_support)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/KingVj01)
+[Donate Me](https://razorpay.me/@MdiskBlast)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
